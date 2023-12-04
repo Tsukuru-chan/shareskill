06318fbc299389cfe36fef2123a57457e8795811
Tsukuru-chan <130873891+Tsukuru-chan@users.noreply.github.com>
2023-12-04 09:40:29 +0900

    Add files via upload

--- model.py
+++ model.py
@@ -31,3 +31,5 @@ def predict(img):
     sorted_prob, sorted_indices = torch.sort(y_prob, descending=True)  # 降順にソート
     return [(classes[idx], prob.item()) for idx, prob in zip(sorted_indices, sorted_prob)]
 
+# Resnetを転移学習
+#http://pchun.work/resnet%E3%82%92fine-tuning%E3%81%97%E3%81%A6%E8%87%AA%E5%88%86%E3%81%8C%E7%94%A8%E6%84%8F%E3%81%97%E3%81%9F%E7%94%BB%E5%83%8F%E3%82%92%E5%AD%A6%E7%BF%92%E3%81%95%E3%81%9B%E3%82%8B/
